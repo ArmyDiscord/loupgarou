986a2b8730f2da0cb56ba8f0219f7980f157c9ea
ArmyBotDiscord <40525896+ArmyBotDiscord@users.noreply.github.com>
2018-08-16 03:18:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+var prefix = "lg!";
+
+client.login("NDcwNDkyMzQ3NDk1NjEyNDQ2.DlZekA.TvPgra46hczn3jI-OwDvelspQqI");
+
+client.on("ready", () => {
+
+    console.log("Je suis prêt !");
+    client.user.setGame("Loup Garou [lg!play]");
+
+});
+
+client.on('message', async message => { 
+
+       if (message.content.startsWith(prefix + "play")) {
+          let replies = ["1", "2", "3", "4", "5", "6"];
+          let result = Math.floor((Math.random() * replies.length));
+      
+          message.delete().catch(O_o => {});
+      
+          try {
+              let newembed = new Discord.RichEmbed()
+                  .setAuthor("Un dé a été lancé !")
+                  .setColor("RANDOM")
+                  .setFooter("Roll - Loup Garou")
+                  .setDescription("Roulé par : " + message.author.username + "\nRésultat : " + replies[result]);
+      
+              message.channel.send({
+                  embed: newembed
+              });
+          } catch (e) {
+              console.log(e.stack);
+          };
+      };
+});
+
